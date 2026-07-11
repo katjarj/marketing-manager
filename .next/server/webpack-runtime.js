f60e8d280eeaecb069e720f52e280bd6bd5d3fb4
ellailan <pixelpoodle@gmail.com>
2026-07-11 10:19:12 -0700

    readme updates

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f9cc8ba54ab67d58")
+/******/ 		__webpack_require__.h = () => ("930bb6d552e643f6")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
